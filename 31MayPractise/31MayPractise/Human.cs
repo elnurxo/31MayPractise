c235115e8dac02c3edf0bb7885c0788a0c38ba52
elnurxo <elnurykh@code.edu.az>
2022-06-03 10:38:28 +0400

    31 may practise

---
+++ 31MayPractise/31MayPractise/Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _31MayPractise
+{
+    internal class Human
+    {
+        public Human(string fullname)
+        {
+            Name = fullname.Substring(0, fullname.IndexOf(' '));
+            Surname = fullname.Substring(fullname.IndexOf(' ') + 1);
+        }
+
+        public string Name;
+        public string Surname;
+        public byte Age; 
+        
+        public string GeneratePassword()
+        {
+            int currentYear = 2022;
+
+            string password = Name.Substring(0, 2).ToUpper() + Surname.Substring(0, 2).ToUpper() + (currentYear - Age);
+
+            return password;
+        }
+    }
+}
---
+++ 31MayPractise/31MayPractise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _31MayPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human("Elnur Khalilov") { Age = 21 };
+
+            Console.WriteLine(human1.Name);
+            Console.WriteLine(human1.Surname);
+
+
+            Console.WriteLine(human1.GeneratePassword());
+
+
+
+            string sentence = "salam p227 telebeleri necesiniz";
+
+            string[] arr = sentence.Split(' ');
+            int count = 0;
+            int digitCounter = 0;
+            foreach (var item in arr)
+            {
+                //if (item.Contains('a'))
+                //{
+                //    count++;
+                //}
+
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (Char.IsDigit(item[i]))
+                    {
+                        digitCounter++;
+                    }
+                }
+            }
+
+
+
+            Console.WriteLine(count);
+            Console.WriteLine(digitCounter);
+            Console.WriteLine(arr.Length);
+        }
+    }
+}
